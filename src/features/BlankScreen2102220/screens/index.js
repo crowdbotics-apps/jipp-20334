b22e23e9d51874112673b143dffccaa28c67098c
Crowdbotics <team@crowdbotics.com>
2020-09-16 16:45:22 +0000

    Update components

--- src/features/BlankScreen2102220/screens/index.js
+++ src/features/BlankScreen2102220/screens/index.js
@@ -25,6 +25,7 @@ export class __Blank extends React.Component {
     Datepicker_13: new Date("01/01/2000"),
     CheckBox_21: false,
     CheckBox_26: false,
+    CheckBox_31: false,
     CheckBox_24: false,
     CheckBox_29: false
   }
@@ -343,6 +344,39 @@ export class __Blank extends React.Component {
             checked={this.state.CheckBox_26}
             onChange={nextChecked =>
               this.setState({ CheckBox_26: nextChecked })
+            }
+          />
+          <CheckBox
+            text="Opposite"
+            disabled={false}
+            style={{
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 5,
+              marginBottom: 5,
+              paddingLeft: 5,
+              paddingRight: 5,
+              paddingTop: 5,
+              paddingBottom: 5,
+              overflow: "visible",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0,
+              textAlign: "left",
+              textAlignVertical: "center",
+              textDecorationLine: "none",
+              textTransform: "none",
+              lineHeight: 12,
+              letterSpacing: 0
+            }}
+            checked={this.state.CheckBox_31}
+            onChange={nextChecked =>
+              this.setState({ CheckBox_31: nextChecked })
             }
           />
         </View>
